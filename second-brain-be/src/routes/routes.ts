d972ad8a14e11a704dc23a7668a0835b46f17834
Aaidt <aadits2004@gmail.com>
2025-03-31 22:49:18 +0530

    backend routing done.

--- second-brain-be/src/routes/routes.ts
+++ second-brain-be/src/routes/routes.ts
@@ -12,6 +12,17 @@ import jwt from 'jsonwebtoken';
 app.use(express.json());
 app.use(cors())
 
+const hashLink = (len: number) => {
+    const randomString = "sdajvuwrnklvuaadASUDGUABFWIONTWE"
+    let hash = ""
+    const length = randomString.length;
+    for (let i = 0; i < len; i++) {
+        hash += randomString[Math.floor((Math.random() * length))]
+    }
+    return hash
+}
+
+
 async function hashPassword(password: string): Promise<string> {
     const saltRounds = 10;
     return await hash(password, saltRounds)
@@ -109,13 +120,70 @@ app.delete("/api/v1/second-brain/content", userMiddleware, async (req: Request, 
 
 app.post("/api/v1/second-brain/share", userMiddleware, async (req: Request, res: Response) => {
     const { share } = req.body;
-    if(share){
-
-    } 
+    if (share) {
+        const link = hashLink(20);
+        const existingLink = await LinkModel.findOne({
+            userId: req.userId
+        })
+        if (existingLink) {
+            res.json({
+                link: existingLink.hash
+            })
+            return;
+        }
+        await LinkModel.create({
+            userId: req.userId,
+            hash: link
+        })
+        res.json({
+            link
+        })
+    }else{
+        await LinkModel.deleteMany({
+            userId: req.userId
+        })
+        res.json({
+            message: "Removed link"
+        })
+    }
 })
 
 app.post("/api/v1/second-brain/:shareLink", userMiddleware, async (req: Request, res: Response) => {
+    const link = req.params.shareLink
+    if(!link){
+        res.json({
+            message: "Link is required."
+        })
+        return
+    }
 
+    const hashLink = await LinkModel.findOne({
+        hash: link
+    })
+    if(!hashLink){
+        res.json({
+            message: "Invalid link."
+        })
+        return 
+    }
+
+    const content = await ContentModel.find({
+        userId: hashLink.userId
+    })
+
+    const user = await UserModel.findOne({
+        _id: hashLink.userId
+    })
+    if(!user){
+        res.json({
+            message: "User not found."
+        })
+    }
+
+    res.json({
+        username: user?.username,
+        content
+    })
 })
 
 const PORT = process.env.PORT as unknown as number
