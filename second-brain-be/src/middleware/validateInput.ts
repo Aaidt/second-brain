cde853f3e5e48b76352d467d2907ad3aa486e6c4
Aaidt <aadits2004@gmail.com>
2025-03-31 20:04:05 +0530

    zod validation middleware updated.

--- second-brain-be/src/middleware/validateInput.ts
+++ second-brain-be/src/middleware/validateInput.ts
@@ -1,17 +1,28 @@
 import { z } from "zod";
+import express, { Request, Response, NextFunction } from "express";
 
-export const ValidateInput = () => {
-    const inputSchema = z.object({
-        username: z.string().min(8, 
-            {
-                message: "username must be atleast 8 characters long."
-            }).email({
-                message: "Invalid email address."
-            }),
-    
-        password: z.string().min(8, {
-            message: "password must be atleast 8 characters long."
+const inputSchema = z.object({
+    username: z.string().min(8, 
+        {
+            message: "username must be atleast 8 characters long."
+        }).email({
+            message: "Invalid email address."
+        }),
+
+    password: z.string().min(8, {
+        message: "password must be atleast 8 characters long."
+    })
+});
+
+export const validateInput = (req: Request, res: Response, next: NextFunction) => {
+   const result = inputSchema.safeParse(req.body);
+
+   if(!result.success){
+        return res.status(400).json({
+            error: result.error.format()
         })
-    });
+   }
+
+   next();
 }
 
