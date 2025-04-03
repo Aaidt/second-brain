7b1a83698b3b7e81225474f5d31ecfdc32afa286
Aaidt <aadits2004@gmail.com>
2025-04-03 20:18:05 +0530

    added border

--- second-brain-fe/src/pages/Signup.tsx
+++ second-brain-fe/src/pages/Signup.tsx
@@ -2,13 +2,13 @@ import { Button } from "../components/ui/Button"
 
 export const Signup = () => {
     return <div className="h-screen w-screen text-[#DDA853] bg-[#F5EEDC] flex justify-center pt-20  ">
-        <div className="w-96 h-110 bg-[#183B4E] rounded-md p-5 pt-10">
+        <div className="w-96 h-100 bg-[#183B4E] rounded-md p-5 pt-25">
             <div>
-                <input type="text" className="w-full m-2 rounded-md "
+                <input type="text" className="w-full rounded-md p-2 border"
                     placeholder="Enter the username..." />
             </div>
             <div>
-                <input type="text" className="w-full m-2 rounded-md "
+                <input type="text" className="w-full  rounded-md p-2 border"
                     placeholder="Enter the password..." />
             </div>
 
