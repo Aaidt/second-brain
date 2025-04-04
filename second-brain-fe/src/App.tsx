454605470927fe0ac664ffaefcf387621ec0a65f
Aaidt <aadits2004@gmail.com>
2025-04-04 20:15:35 +0530

    routing done.

--- second-brain-fe/src/App.tsx
+++ second-brain-fe/src/App.tsx
@@ -1,12 +1,22 @@
 // import { useState } from 'react'
 import { Dashboard } from "./pages/Dashboard"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Signup } from "./pages/Signup"
+import { Signin } from "./pages/Signin"
 
 function App() {
 
   return (
-    // <Dashboard />
-    <Signup />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Signup />}></Route>
+      <Route path="/Signup" element={<Signup />}></Route>
+      <Route path="/Signin" element={<Signin />}></Route>
+      <Route path="/Dashboard" element={<Dashboard />}></Route>
+    </Routes>
+    
+    </BrowserRouter>
+
   )
 }
 
--- second-brain-fe/src/pages/Signin.tsx
+++ second-brain-fe/src/pages/Signin.tsx
@@ -1,15 +1,20 @@
 import { Button } from "../components/ui/Button"
 
 export const Signin = () => {
-    return <div className="h-screen w-screen bg-[#F5EEDC]">
-        <div className="w-96 h-124 bg-[#183B4E] rounded-md">
-            <input type="text" className="w-full m-2 rounded-md border"
-                placeholder="Enter the username..." />
+    return <div className="h-screen w-screen text-[#DDA853] bg-[#F5EEDC] flex justify-center pt-20  ">
+        <div className="w-96 h-100 bg-[#183B4E] rounded-md p-5 pt-25">
+            <div>
+                <input type="text" className="w-full rounded-md p-2 border"
+                    placeholder="Enter the username..." />
+            </div>
+            <div>
+                <input type="text" className="w-full  rounded-md p-2 border"
+                    placeholder="Enter the password..." />
+            </div>
 
-            <input type="text" className="w-full m-2 rounded-md border"
-                placeholder="Enter the password..." />
-
-            <Button size="md" text="Signin" bg_color="gold" fullWidth={true} />
+            <div>
+                <Button size="md" text="Signin" bg_color="gold" fullWidth={true} />
+            </div>
         </div>
     </div>
 } 
