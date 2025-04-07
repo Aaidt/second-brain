da63955966a927d3f4d7f31f936b0ec347d8e9b8
Aaidt <aadits2004@gmail.com>
2025-04-07 13:32:23 +0530

    prisma not working

--- second-brain-be/src/db/db.ts
+++ second-brain-be/src/db/db.ts
@@ -1,35 +1,35 @@
-// import mongoose, { Schema, model } from "mongoose"
-// import dotenv from "dotenv";
-// dotenv.config();
+import mongoose, { Schema, model } from "mongoose"
+import dotenv from "dotenv";
+dotenv.config();
 
-// const mongoURL = process.env.CONN as string
-// if(!mongoURL){
-//     throw new Error("MongoDB connection string is missing in the env file.")
-// }
+const mongoURL = process.env.CONN as string
+if(!mongoURL){
+    throw new Error("MongoDB connection string is missing in the env file.")
+}
 
-// mongoose.connect(mongoURL);
+mongoose.connect(mongoURL);
 
 
-// const UserSchema = new Schema({
-//     username: { type: String, unique: true },
-//     password: String,
-// })
+const UserSchema = new Schema({
+    username: { type: String, unique: true },
+    password: String,
+})
 
-// export const UserModel = model("Users", UserSchema);
+export const UserModel = model("Users", UserSchema);
 
-// const ContentSchema = new Schema({
-//     title: String,
-//     link: String,
-//     // tags: [{ type: mongoose.Types.ObjectId }]
-//     type: String,
-//     userId: { type: mongoose.Types.ObjectId, ref: "Users", required: true }
-// })
+const ContentSchema = new Schema({
+    title: String,
+    link: String,
+    // tags: [{ type: mongoose.Types.ObjectId }]
+    type: String,
+    userId: { type: mongoose.Types.ObjectId, ref: "Users", required: true }
+})
 
-// export const ContentModel = model("Contents", ContentSchema);
+export const ContentModel = model("Contents", ContentSchema);
 
-// const LinkSchema = new Schema({
-//     hash: String,
-//     userId: { type: mongoose.Types.ObjectId, ref: "Users", required: true }
-// })
+const LinkSchema = new Schema({
+    hash: String,
+    userId: { type: mongoose.Types.ObjectId, ref: "Users", required: true }
+})
 
-// export const LinkModel = model("Links", LinkSchema)
+export const LinkModel = model("Links", LinkSchema)
