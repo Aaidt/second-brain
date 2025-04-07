4fb3f7440e6d8bdc4a1e0630f9ca3727cd12f9cb
Aaidt <aadits2004@gmail.com>
2025-04-07 15:49:40 +0530

    icons added to teh card component.

--- second-brain-fe/src/components/ui/CardComponent.tsx
+++ second-brain-fe/src/components/ui/CardComponent.tsx
@@ -1,7 +1,16 @@
+import { Delete } from "../icons/Delete"
+import { Page } from "../icons/Page"
+import { ShareIcon } from "../icons/ShareIcon"
 
 
 export const CardComponent = () => {
     return <div className="w-72 h-96 bg-[#183B4E] rounded-md ">
-
+        <div className="flex justify-between p-5">
+            <Page />
+            <div className="flex gap-4">
+                <Delete />
+                <ShareIcon />
+            </div>
+        </div>
     </div>
 }
