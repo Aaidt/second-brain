591520f00a6c053bbdf0de4cc92c3d39028f62bf
Aaidt <aadits2004@gmail.com>
2025-04-08 20:50:42 +0530

    sidebar transition fixed.

--- client/src/components/ui/Sidebar.tsx
+++ client/src/components/ui/Sidebar.tsx
@@ -1,45 +1,42 @@
 import { useState } from "react";
-import { BrainIcon } from "../icons/BrainIcon"
-import { CloseBarIcon } from "../icons/CloseBarIcon"
-import { OpenBarIcon } from "../icons/OpenBarIcon"
-import { TwitterIcon } from "../icons/TwitterIcon"
-import { YoutubeIcon } from "../icons/YoutubeIcon"
-import { RedditIcon } from "../icons/RedditIcon"
-import { SideBarItems } from "./SideBarItems"
-
+import { BrainIcon } from "../icons/BrainIcon";
+import { CloseBarIcon } from "../icons/CloseBarIcon";
+import { OpenBarIcon } from "../icons/OpenBarIcon";
+import { TwitterIcon } from "../icons/TwitterIcon";
+import { YoutubeIcon } from "../icons/YoutubeIcon";
+import { RedditIcon } from "../icons/RedditIcon";
+import { SideBarItems } from "./SideBarItems";
 
 export const Sidebar = () => {
+  const [sidebarClose, setSidebarClose] = useState(false);
 
-    const [sidebarClose, setSidebarClose] = useState(false)
+  return (
+    <div
+      className={`h-screen bg-[#183B4E] border-black rounded-r transition-all duration-300 ease-in-out ${
+        sidebarClose ? "w-15" : "w-72"
+      }`}
+    >
+      <div
+        className={`p-3 hover:scale-105 cursor-pointer transition-transform duration-200 ${
+          sidebarClose ? "text-center" : ""
+        }`}
+        onClick={() => setSidebarClose(!sidebarClose)}
+      >
+        {sidebarClose ? <OpenBarIcon /> : <CloseBarIcon />}
+      </div>
 
-    return <div className="">
-        {sidebarClose ? (
-            <div className="h-screen transition-all hover:p-1 bg-[#183B4E]">
-                <div onClick={() => setSidebarClose(!sidebarClose)}
-                    className="pt-3 hover:translate-x-1">
-                    {<OpenBarIcon />}
-                </div>
-            </div>
-        ) : (
-            <div className=" transition duration-400">
-                <div className="h-screen bg-[#183B4E] w-72 border-black rounded-r">
-                    <div className="">
-                        <div
-                            className="p-3 hover:-translate-x-1 duration-200"
-                            onClick={() => setSidebarClose(!sidebarClose)}>
-                            {<CloseBarIcon />}
-                        </div>
-                        <div className="flex p-2 font-bold text-3xl items-center gap-3">
-                            {<BrainIcon />}  Second Brain
-                        </div>
-                    </div>
-                    <div>
-                        {<SideBarItems text="Twitter" icon={<TwitterIcon />} />}
-                        {<SideBarItems text="YouTube" icon={<YoutubeIcon />} />}
-                        {<SideBarItems text="Reddit" icon={<RedditIcon />} />}
-                    </div>
-                </div>
-            </div>
-        )}
+      {!sidebarClose && (
+        <>
+          <div className="flex p-2 font-bold text-3xl text-[#F5EEDC] items-center gap-3 text-white">
+            <BrainIcon /> Second Brain
+          </div>
+          <div>
+            <SideBarItems text="Twitter" icon={<TwitterIcon />} />
+            <SideBarItems text="YouTube" icon={<YoutubeIcon />} />
+            <SideBarItems text="Reddit" icon={<RedditIcon />} />
+          </div>
+        </>
+      )}
     </div>
-}
+  );
+};
