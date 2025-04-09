66a4bf46c26ab040ab435265ff1c734fd157f9b3
Aaidt <aadits2004@gmail.com>
2025-04-09 16:03:18 +0530

    content modal changes.

--- client/src/components/ui/createContentModal.tsx
+++ client/src/components/ui/createContentModal.tsx
@@ -1,10 +1,17 @@
 export const CreateContentModal = () => {
     return <div className="flex justify-center items-center pt-20">
-        <div className="bg-[#183B4E] rounded-md min-w-72 h-96 flex justify-center items-center text-[#DDA853]">
-            <div className="font-bold font-xl">
-                Add Content
+        <div className="bg-[#183B4E] rounded-md min-w-90 h-96 p-5 text-[#DDA853]">
+            <div className="font-bold text-4xl pb-6  flex justify-center">Add Content</div>
+            <div className="p-1">
+                <div className="m-4">
+                    <input type="text" className="w-full rounded-md p-2 border"
+                        placeholder="Title..." />
+                </div>
+                <div className="m-4">
+                    <input type="text" className="w-full rounded-md p-2 border"
+                        placeholder="Link..." />
+                </div>
             </div>
-
         </div>
     </div>
 
