a5224c0bd7778d06b342c333fd7247f822e84052
Aaidt <aadits2004@gmail.com>
2025-04-09 22:43:50 +0530

    add content modal styling.

--- client/src/components/ui/createContentModal.tsx
+++ client/src/components/ui/createContentModal.tsx
@@ -2,38 +2,45 @@ import { useState } from "react";
 import { Button } from "./Button"
 import { DropDownMenu } from "./dropdown-menu"
 
-export const CreateContentModal = () => {
+type modalProps = {
+    open: Boolean
+}
+
+export const CreateContentModal = ({ open }: modalProps) => {
     const [selectedVal, setSelectedVal] = useState("");
 
-    return <div className="flex justify-center items-center pt-20">
-        <div className="bg-[#183B4E] rounded-md min-w-90 h-96 p-5 text-[#DDA853]">
-            <div className="font-bold text-4xl pb-6  flex justify-center">Add Content</div>
-            <div className="p-1">
-                <div className="m-4">
-                    <input type="text" className="w-full rounded-md p-2 border"
-                        placeholder="Title..." />
-                </div>
-                <div className="m-4">
-                    <input type="text" className="w-full rounded-md p-2 border"
-                        placeholder="Link..." />
-                </div>
+    return (open && <div>
+        <div className="fixed top-0 left-0 h-screen w-screen bg-black opacity-60"></div>
+            <div className="flex justify-center items-center h-screen">
+                <div className="bg-[#183B4E] rounded-md min-w-90 h-96 p-5 text-[#DDA853]">
+                    <div className="font-bold text-4xl pb-6  flex justify-center">Add Content</div>
+                    <div className="p-1">
+                        <div className="m-4">
+                            <input type="text" className="w-full rounded-md p-2 border"
+                                placeholder="Title..." />
+                        </div>
+                        <div className="m-4">
+                            <input type="text" className="w-full rounded-md p-2 border"
+                                placeholder="Link..." />
+                        </div>
 
-                <DropDownMenu
-                    options={[
-                        { label: "YouTube", value: "youtube" },
-                        { label: "X", value: "twitter" },
-                        { label: "Reddit", value: "reddit" },
-                        { label: "Others", value: "others" },
-                    ]}
-                    onSelect={(val) => setSelectedVal(val)}
-                />
+                        <DropDownMenu
+                            options={[
+                                { label: "YouTube", value: "youtube" },
+                                { label: "X", value: "twitter" },
+                                { label: "Reddit", value: "reddit" },
+                                { label: "Others", value: "others" },
+                            ]}
+                            onSelect={(val) => setSelectedVal(val)}
+                        />
 
-                <div className="font-bold">
-                    <Button size="md" text="Submit" bg_color="gold" fullWidth={true} />
+                        <div className="font-bold">
+                            <Button size="md" text="Submit" bg_color="gold" fullWidth={true} />
+                        </div>
+
+                    </div>
                 </div>
-
             </div>
         </div>
-    </div>
-
+    )
 }
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -11,39 +11,36 @@ export const Dashboard = () => {
 
     return (
         <>
-            {
-                modalOpen ? (
-                    <CreateContentModal />
-                ) : (
-                    <div className="bg-[#F5EEDC] flex font-serif text-[#DDA853]">
-                        <div>
-                            <Sidebar />
+            <CreateContentModal open={modalOpen} />
+            <div className="bg-[#F5EEDC] flex font-serif text-[#DDA853]">
+                <div>
+                    <Sidebar />
+                </div>
+                <div className="flex p-4 flex-wrap">
+                    <div className="pt-25 p-6">{<CardComponent title="asd" type="youtube" link="https://www.youtube.com/watch?v=q_edsSpDzHg&t=1192s" />}</div>
+                    <div className="pt-25 p-6">{<CardComponent title="asd" type="twitter" link="https://x.com/KevinVanCott/status/1909222844891865105" />}</div>
+                    <div className="pt-25 p-6">{<CardComponent title="asd" type="reddit" link="https://www.reddit.com/r/IndianWorkplace/comments/1juzqw8/my_manager_backstabbed_me/" />}</div>
+                    <div className="pt-5 p-3 fixed right-0 top-0 flex">
+                        <div className="">
+                            <Button
+                                size="md" text="Add content" bg_color="blue"
+                                fullWidth={false} startIcon={<PlusIcon />}
+                                onClick={() => setModalOpen(true)}
+                            />
                         </div>
-                        <div className="flex p-4 flex-wrap">
-                            <div className="pt-25 p-6">{<CardComponent title="asd" type="youtube" link="https://www.youtube.com/watch?v=q_edsSpDzHg&t=1192s" />}</div>
-                            <div className="pt-25 p-6">{<CardComponent title="asd" type="twitter" link="https://x.com/KevinVanCott/status/1909222844891865105" />}</div>
-                            <div className="pt-25 p-6">{<CardComponent title="asd" type="reddit" link="https://www.reddit.com/r/IndianWorkplace/comments/1juzqw8/my_manager_backstabbed_me/" />}</div>
-                            <div className="pt-5 p-3 fixed right-0 top-0 flex">
-                                <div className="">
-                                    <Button
-                                        size="md" text="Add content" bg_color="blue"
-                                        fullWidth={false} startIcon={<PlusIcon />}
-                                        onClick={() => setModalOpen(true)}
-                                    />
-                                </div>
-                                <div className="">
-                                    <Button
-                                        size="md" text="Share Brain" bg_color="blue"
-                                        fullWidth={false} startIcon={<ShareIcon style='float' />}
-                                    />
-                                </div>
-                            </div>
+                        <div className="">
+                            <Button
+                                size="md" text="Share Brain" bg_color="blue"
+                                fullWidth={false} startIcon={<ShareIcon style='float' />}
+                            />
                         </div>
-
-
                     </div>
-                )
-            }
+                </div>
+
+
+            </div>
+
+
         </>
     )
 }
