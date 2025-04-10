361e27ebdbecb57efca1eff2e784419c3c5f2f19
Aaidt <aadits2004@gmail.com>
2025-04-10 21:39:43 +0530

    logging out if the jwt expires.

--- client/src/pages/ProtectedRoute.tsx
+++ client/src/pages/ProtectedRoute.tsx
@@ -1,11 +1,20 @@
 import { Navigate } from "react-router-dom"
 
 export const ProtectedRoute = ({ children }: { children: React.JSX.Element }) => {
-    const token = localStorage.getItem("authorization");
 
-    if (!token) {
+    const token = localStorage.getItem("authorization");
+    const payload = token ? JSON.parse((atob(token.split('.')[1]))) : undefined
+    const expiry = payload.exp
+
+    const isVerified = (token: string) => {
+        if (!token) return false;
+        const now = Math.floor(Date.now() / 1000);
+        return now < expiry
+    }
+
+    if (!isVerified) {
         return <Navigate to="/signin" replace />
     }
 
-    return children 
+    return children
 }
