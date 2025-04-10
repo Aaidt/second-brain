e34c79c2cff9dc97d969aa72761d05959da507ee
Aaidt <aadits2004@gmail.com>
2025-04-10 15:10:38 +0530

    secure routing to dashboard.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Dashboard } from "./pages/Dashboard"
 import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom"
 import { Signup } from "./pages/Signup"
 import { Signin } from "./pages/Signin"
+import { ProtectedRoute } from "./pages/ProtectedRoute"
 
 function App() {
 
@@ -12,7 +13,14 @@ function App() {
         <Route path="/" element={<Signup />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/signin" element={<Signin />}></Route>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }>
+        </Route>
       </Routes>
     </BrowserRouter>
 
--- client/src/components/ui/CardComponent.tsx
+++ client/src/components/ui/CardComponent.tsx
@@ -47,7 +47,7 @@ export const CardComponent = (cardProps: cardPropTypes) => {
                         frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                         referrerPolicy="strict-origin-when-cross-origin"
-                        className="rounded-lg"
+                        className="rounded-lg m-4"
                         allowFullScreen
                     />
                     </div>
