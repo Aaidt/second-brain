5f5c1dfee5a87ff1ee12e74369fb53e25a52b5b4
Aaidt <aadits2004@gmail.com>
2025-04-21 10:50:38 +0530

    hover styles for navbar.

--- client/src/components/ui/Navbar.tsx
+++ client/src/components/ui/Navbar.tsx
@@ -2,21 +2,21 @@ import { BrainIcon } from "../icons/BrainIcon"
 
 
 export const Navbar = () => {
-    return <div className="flex justify-center justify-between p-2 bg-[#DDA853] text-[#183B4E] rounded-full w-180 h-18 shadow-xl shadow-gray-800 border-b-black border-r-black">
+    return <div className="flex justify-center justify-between p-2 bg-[#DDA853] text-[#183B4E] rounded-full w-180 h-18 shadow-xl shadow-gray-800 hover:shadow-gray-900 border-b-black border-r-black">
         <div className="flex pr-70">
             <div className="pl-1 p-1">
                 <BrainIcon />
             </div>
-            <div className="p-2 font-semibold flex justify-center items-center pb-2 text-xl">
+            <div className="p-2 font-bold flex justify-center items-center pb-2 text-xl">
                 Second <br /> Brain
             </div>
         </div>
 
         <div className="flex p-4 pl-30">
-            <div className="font-light text-xl pr-8">
+            <div className="font-light text-xl pr-8 hover:underline hover:underline-offset-5 hover:-translate-y-1 duration-200">
                 Login
             </div>
-            <div className="font-light text-xl ">
+            <div className="font-light text-xl hover:underline hover:underline-offset-5 hover:-translate-y-1 duration-200 ">
                 Signup
             </div>
         </div>
