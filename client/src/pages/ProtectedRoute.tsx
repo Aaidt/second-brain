9ca291c2040f89b158c5b896b711981de8d4ea84
Aaidt <aadits2004@gmail.com>
2025-04-27 14:10:24 +0530

    no need for jwt verification on fe.
    (it doesnt work in browser anyway.)

--- client/src/pages/ProtectedRoute.tsx
+++ client/src/pages/ProtectedRoute.tsx
@@ -1,24 +1,24 @@
 import { Navigate } from "react-router-dom";
 
 export const ProtectedRoute = ({ children }: { children: React.JSX.Element }) => {
-  const token = localStorage.getItem("authorization");
+    const token = localStorage.getItem("authorization");
 
-  if (!token) {
-    alert("Login first!!!");
-    console.log("No JWT token found");
-    return <Navigate to="/signin" />;
-  }
+    if (!token) {
+        alert("Login first!!!");
+        console.log("No JWT token found");
+        return <Navigate to="/signin" />;
+    }
 
-  const payload = JSON.parse(atob(token.split(".")[1]));
-  const expiry = payload.exp;
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    const expiry = payload.exp;
 
-  const now = Math.floor(Date.now() / 1000);
-  const isExpired = now > expiry;
+    const now = Math.floor(Date.now() / 1000);
+    const isExpired = now > expiry;
 
-  if (isExpired) {
-    alert("Session expired! Please log in again.");
-    return <Navigate to="/signin" />;
-  }
+    if (isExpired) {
+        alert("Session expired! Please log in again.");
+        return <Navigate to="/signin" />;
+    }
 
-  return children;
+    return children;
 };
