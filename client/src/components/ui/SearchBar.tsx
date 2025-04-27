605e75814dc1946063806d4fd493cf06e83c8985
Aaidt <aadits2004@gmail.com>
2025-04-27 11:23:36 +0530

    Landing page.

--- client/src/components/ui/SearchBar.tsx
+++ client/src/components/ui/SearchBar.tsx
@@ -8,7 +8,7 @@ export const SearchBar = () => {
 
     return <div className="text-[#183B4E] pt-1 flex hover:text-[#27548A]">
         {/* <Button size="md" text="Search" bg_color="gold" fullWidth={false} shadow={false} startIcon={<SearchPageIcon />} /> */}
-        <input type="text" placeholder="Search..." className={`${inputOpen ? 'w-42' : 'hidden'} ease-in-out duration-300 border-2 rounded-md hover:border-[#27548A] border-[#183B4E]`} />
+        <input type="text" placeholder="Search..." className={`${inputOpen ? 'w-42' : 'hidden'} transition-all ease-in-out duration-300 border-2 rounded-md hover:border-[#27548A] border-[#183B4E]`} />
 
         {inputOpen ? (<SearchIcon />) : 
             (<div onClick={() => setInputOpen(!inputOpen)}>
