232c7c41ae4672c7d39a25f68c90b401b5e1a8c3
Aaidt <aadits2004@gmail.com>
2025-04-27 09:47:57 +0530

    searchbar opening styles.

--- client/src/components/ui/SearchBar.tsx
+++ client/src/components/ui/SearchBar.tsx
@@ -1,13 +1,21 @@
-// import { SearchIcon } from "../icons/SearchIcon"
+import { SearchIcon } from "../icons/SearchIcon"
 // import { Button } from "./Button"
 import { SearchPageIcon } from "../icons/SearchPageIcon"
+import { useState } from "react";
 
 export const SearchBar = () => {
+    const [inputOpen, setInputOpen] = useState(false)
+
     return <div className="text-[#183B4E] pt-1 flex hover:text-[#27548A]">
         {/* <Button size="md" text="Search" bg_color="gold" fullWidth={false} shadow={false} startIcon={<SearchPageIcon />} /> */}
-        <input type="text" placeholder="Search..." className="border-2 rounded-md hover:border-[#27548A] border-[#183B4E]" />
-        <SearchPageIcon />
-        {/* <input type="text" placeholder="Search..."></input>
+        <input type="text" placeholder="Search..." className={`${inputOpen ? 'w-42' : 'hidden'} ease-in-out duration-300 border-2 rounded-md hover:border-[#27548A] border-[#183B4E]`} />
+
+        {inputOpen ? (<SearchIcon />) : 
+            (<div onClick={() => setInputOpen(!inputOpen)}>
+            <SearchPageIcon />
+        </div>)
+        }
+    {/* <input type="text" placeholder="Search..."></input>
         <SearchIcon /> */}
-    </div>
+    </div >
 }
