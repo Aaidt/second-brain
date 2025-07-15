e5b55c0320760c1de18ed35c128df14240040e7d
Aaidt <aadits2004@gmail.com>
2025-07-15 23:37:13 +0530

    cors env

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,7 +18,7 @@ const app = express()
 app.use(express.json())
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:5173"
+    origin: process.env.FRONTEND_URL
 }))
 app.use(cookieParser())
 
