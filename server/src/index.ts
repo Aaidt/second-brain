a1e56d881ce2aaa8fdec5b938c14fa9f7db10f51
Aadit <aadits2004@gmail.com>
2025-09-20 20:53:52 +0530

    added multithreading using the cluster module

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,31 +9,49 @@ import thoughtRouter from "./routes/thoughtRouter"
 import chatMessageRouter from "./routes/chatMessageRouter"
 import chatSessionRouter from "./routes/chatSessionRouter"
 import linkRouter from "./routes/linkRouter"
-
+import cluster from 'cluster'
+import os from "os"
 
 dotenv.config()
 
-const app = express()
+if (cluster.isMaster) {
+   const number_of_cpus = os.cpus().length;
+   console.log(`Master ${process.pid} is running.`)
 
-app.use(express.json())
-app.use(cors({
-   credentials: true,
-   allowedHeaders: ["Content-Type", "Authorization"],
-   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-   origin: ["https://second-brainfe.vercel.app", "https://second-brain.codexbuild.website", "http://localhost:5173"]
-}))
-app.use(cookieParser())
+   for (let i = 0; i < number_of_cpus; i++) {
+      cluster.fork();
+   }
 
-app.use("/second-brain/api/auth", authRouter)
-app.use("/second-brain/api/content", userMiddleware, contentRouter)
-app.use("/second-brain/api/thought", userMiddleware, thoughtRouter)
-app.use("/second-brain/api/link", userMiddleware, linkRouter)
-app.use("/second-brain/api/chatMessage", userMiddleware, chatMessageRouter)
-app.use("/second-brain/api/chatSession", userMiddleware, chatSessionRouter)
+   cluster.on("exit", (worker, code, signal) => {
+      console.log(`Worker ${worker.process.pid} has died`);
+      console.log("Starting a new worker...");
+      cluster.fork();
+   })
+} else {
 
-app.get("/healthcheck", (req: Request, res: Response) => {
-   res.status(200).json({ message: "The VM is healthy." })
-})
+   const app = express()
 
-app.listen(3000, () => { console.log('Server is listening on port 3000.') })
+   app.use(express.json())
+   app.use(cors({
+      credentials: true,
+      allowedHeaders: ["Content-Type", "Authorization"],
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+      origin: ["https://second-brainfe.vercel.app", "https://second-brain.codexbuild.website", "http://localhost:5173"]
+   }))
+   app.use(cookieParser())
+
+   app.use("/second-brain/api/auth", authRouter)
+   app.use("/second-brain/api/content", userMiddleware, contentRouter)
+   app.use("/second-brain/api/thought", userMiddleware, thoughtRouter)
+   app.use("/second-brain/api/link", userMiddleware, linkRouter)
+   app.use("/second-brain/api/chatMessage", userMiddleware, chatMessageRouter)
+   app.use("/second-brain/api/chatSession", userMiddleware, chatSessionRouter)
+
+   app.get("/healthcheck", (req: Request, res: Response) => {
+      res.status(200).json({ message: "The VM is healthy." })
+   })
+
+   app.listen(3000, () => { console.log('Server is listening on port 3000.') })
+
+}
 
