38b8374c2273857cf508bb31586922710b0206e0
Aadit <aadits2004@gmail.com>
2026-01-01 21:57:34 +0530

    swapped routes

--- serverless/app/api/second-brain/chatSession/create/route.ts
+++ serverless/app/api/second-brain/chatSession/create/route.ts
@@ -1,23 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prismaClient } from "@/lib/prisma";
 
-export async function GET(req: Request){
-    const userId = req.headers.get("x-user-id");
-    if(!userId){
-        return NextResponse.json({
-            message: "user is not authenticated"
-        }, { status: 401 })
-    }
-        
-    try {
-        const sessions = await prismaClient.chatSession.findMany({
-            where: { userId },
-            orderBy: { created_at: "desc" },
-            take: 15
-        });
-        return NextResponse.json({ sessions }, { status: 201 });
-    } catch (err) {
-        console.error("Error is: " + err)
-        return NextResponse.json({ message: "Error fetching sessions." }, { status: 500 });
-    }
+export async function POST(req: Request) {
+  const userId = req.headers.get("x-user-id");
+  const { title } = await req.json();
+  if (!userId) {
+    return NextResponse.json(
+      {
+        message: "user is not authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  if (!title || !userId) {
+    return NextResponse.json({ message: "Title and userId are required." }, { status: 401 });
+}
+
+try {
+    const session = await prismaClient.chatSession.create({
+        data: {
+            title,
+            user: { connect: { id: userId } },
+        },
+    });
+    return NextResponse.json({ session }, { status: 200 });
+} catch (err) {
+    console.error("Error is: " + err)
+    return NextResponse.json({ message: "Error creating session." }, { status: 500 });
+}
+
 }
